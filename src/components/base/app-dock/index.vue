7d9046b15dd33841426a4e79e3bfac1486501f17
bojun1995 <bojun1995@foxmail.com>
2020-11-27 23:37:06 +0800

    '【新增】app-dock'

---
+++ src/components/base/app-dock/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    123123
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppDock',
+  data() {
+    return {
+      
+    }
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" rel="stylesheet/scss" type="text/scss" scoped>
+</style>
